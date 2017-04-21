319fdc72e778191ccb099ad595e997cacc0b11e9
baixin.zhao <baixin.zhao@rokid.com>
2017-04-21 17:04:34 +0800

    add test of session

--- test/session.test.js
+++ test/session.test.js
@@ -1,16 +1,15 @@
 const should = require('chai').should();
 const db = require('../lib/db');
 const Session = require('../lib/session');
-const session = new Session();
 
 describe('session', function() {
-  before(function() {
+  beforeEach(function() {
     const ses = {
       name: 'demo',
       endpoint: 'http://127.0.0.1:3000',
       useAuth:{
-        userId: '',
-        userToken: ''
+        userId: '123',
+        userToken: '123'
       }
     };
     db.get('sessions')
@@ -21,10 +20,57 @@ describe('session', function() {
       .write();
   });
 
-  describe('#session', function() {
+  describe('#getAllSessions()', function() {
+    it('allSessions should be an array', function(done) {
+      Session.getAllSessions().should.be.an('array');
+      done();
+    })
+  });
+
+  describe('#getCurrentSessionName()', function() {
     it('currentSessionName should be a string', function(done) {
-      const currentSessionName = session.currentSessionName;
-      currentSessionName.should.be.a('string');
+      Session.getCurrentSessionName().should.be.a('string');
+      done();
+    })
+  });
+
+  describe('#getCurrentSession()', function() {
+    it('currentSession should be an object', function(done) {
+      Session.getCurrentSession().should.be.a('object');
+      done();
+    })
+  });
+
+  describe('#setCurrentSession()', function() {
+    it('currentSession should be updated', function(done) {
+      Session.setCurrentSession('test');
+      db.get('currentSession').value().should.equal('test');
+      done();
+    })
+  });
+
+  describe('#getSessionByName()', function() {
+    it('session get by name should be an object and name should be equal to input', function(done) {
+      Session.getSessionByName('demo').should.be.an('object');
+      Session.getSessionByName('demo').name.should.equal('demo');
+      done();
+    })
+  });
+
+  describe('#addSession()', function() {
+    it('session should be added to sessions array', function(done) {
+      const test = {
+        name: 'test',
+        endpoint: 'tcp://192.168.0.1:8080',
+        userAuth:{
+          userId: '12345',
+          userToken: '12345'
+        }
+      };
+
+      Session.addSession(test);
+      db.get('sessions').find({name: test.name}).value().should.be.an('object');
+      db.get('sessions').find({name: test.name}).value().name.should.equal('test');
       done();
     })
   })
