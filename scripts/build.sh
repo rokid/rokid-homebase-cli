ec2dce131d1084957963e19f3e030ea2a40a28d4
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-03-26 15:39:01 +0800

    chore: releases

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+TARGETS="alpine-x64 linux-x64 linux-x86 mac-x64"
+for i in $TARGETS; do
+  npx nexe -t $i"-8.9.4" ./index.js -o "./build/rhome-"$TRAVIS_TAG"-"$i
+done
+
+WIN_TARGETS="windows-x64 windows-x86"
+for i in $WIN_TARGETS; do
+  npx nexe -t $i"-8.9.4" ./index.js -o "./build/rhome-"$TRAVIS_TAG"-"$i".exe"
+done
