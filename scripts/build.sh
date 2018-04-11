8eb0f60904825d0b25a3412b5f0edf10a821a928
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-04-11 14:23:35 +0800

    fix: nexe resource inclusion

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 
 TARGETS="alpine-x64 linux-x64 linux-x86 mac-x64"
 for i in $TARGETS; do
-  npx nexe -t $i"-8.9.4" --resource ./jsonschema ./index.js -o "./build/rhome-"$TRAVIS_TAG"-"$i
+  npx nexe -t $i"-8.9.4" --resource './jsonschema/*' ./index.js -o "./build/rhome-"$TRAVIS_TAG"-"$i
 done
 
 WIN_TARGETS="windows-x64 windows-x86"
 for i in $WIN_TARGETS; do
-  npx nexe -t $i"-8.9.4" --resource ./jsonschema ./index.js -o "./build/rhome-"$TRAVIS_TAG"-"$i".exe"
+  npx nexe -t $i"-8.9.4" --resource './jsonschema/*' ./index.js -o "./build/rhome-"$TRAVIS_TAG"-"$i".exe"
 done
